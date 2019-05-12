c2b3af00938e011aeda39f83a34e6a6d7bbfcf68
Jeonghwan Kim <wjdghks950@github.com>
2019-05-13 03:33:23 +0900

    Update ReplayModel - def sample() to return 4 channel states

--- model/model.py
+++ model/model.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import random
+from collections import namedtuple
+
+transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+state = namedtuple('state', ('state_tuple', 'state_img'))
 
 class DQN(nn.Module):
     def __init__(self, n_states, n_actions):
@@ -16,4 +20,22 @@ class DQN(nn.Module):
         out = self.relu(self.fc2(out))
         out = self.relu(self.fc3(out))
         return out
-        #return out.view(out.size(0), -1)
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size, time_step):
+        sample_t = random.sample(list(enumerate(self.memory)), batch_size) # Retrieve (idx, item) tuple from memory
+        for idx, sample in enumerate(sample_t):
+            for j in range(1, time_step+1):
+                sample_t[0][1] = torch.cat(sample_t[0][1], self.memory[idx][0][1]), dim=0)
+        return sample_t
