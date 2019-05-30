1571712f0e923835d04e15c1d7fd13f41ef1c6ec
Jeonghwan Kim <wjdghks950@github.com>
2019-05-30 22:53:29 +0900

    Update agent.py

--- agent.py
+++ agent.py
@@ -10,16 +10,20 @@ import cPickle
 import threading
 import argparse
 from PIL import Image
+from shutil import copyfile
 
 from model.model import DQN
 from tools import print_log
-from tools import ReplayMemory, Transition, state, state_tuple
+from tools import ReplayMemory
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
+import torchvision
+
+
 
 parser = argparse.ArgumentParser(description='Model parameters')
 parser.add_argument('-d', '--discount', default=0.8, type=float, help='Discount value(gamma) for Discounted Return')
@@ -32,12 +36,20 @@ parser.add_argument('--TARGET_UPDATE_CYCLE', default=10, type=int, help='Target_
 parser.add_argument('--path', default='./data/', type=str, help='saved state image data_path')
 parser.add_argument('--SAVE_PATH', default='./log/', type=str, help='')
 parser.add_argument('--MEMORY_SIZE', default=10000, type=int, help='ReplayMemory capacity')
+parser.add_argument('--log', default=None, help='open log')
+parser.add_argument('--TEST_INTERVAL', default=100, type=int, help='evaluation inverval')
 
 args = parser.parse_args()
 
 filename = 'log_{seed}_ymd{y}{m}{d}_hm{h}{min}_model{model}'.format(seed=0, y=0 , m=0, d=0, h=0, min=0, model='rl_parking')
 log = open(os.path.join(args.SAVE_PATH, 'txt'+filename+'.txt'), 'w')
 #log_csv = None
+args.log = log
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+state = namedtuple('state', ('state_img', 'state_tuple'))
+state_tuple = namedtuple('state_tuple', ('x', 'y', 'theta_heading', 's', 'theta_steering'))
+
 
 print_log("pytorch version : {}".format(torch.__version__), log)
 print_log("------[Initial parameters]------", log)
@@ -80,10 +92,11 @@ class LearningAgent(Agent):
             print "using cpu..."
 
         self.memory = ReplayMemory(args.MEMORY_SIZE)
-        self.time_backward = 4
         
         self.policy_net = DQN(vec_size=5, n_actions=9).to(self.device)
         self.target_net = DQN(vec_size=5, n_actions=9).to(self.device)
+#        self.policy_net = DQN(vec_size=5, n_actions=9).cuda()
+#        self.target_net = DQN(vec_size=5, n_actions=9).cuda()
         self.target_net.load_state_dict(self.policy_net.state_dict())
         self.target_net.eval()
         
@@ -94,8 +107,13 @@ class LearningAgent(Agent):
 
         if 1 :
             self.state_path = 'state.png'
+            
+            init_path = os.path.join(args.path, '../env.png')    
+            cp_path = os.path.join(args.path, self.state_path)
+            copyfile(init_path, cp_path)
+            self.env.update_screen()
 
-#    def load_dataset(self, path):
+#    def load_dataset(self, path):i
 #        train_dataset = torchvision.datasets.ImageFolder(
 #            root = path,
 #            transform = T.toTensor()
@@ -109,10 +127,16 @@ class LearningAgent(Agent):
 #        return data_loader
 
     # Retrieve screens saved by car_parking_env.py/captureStates and convert them to tensor
+    '''
     def get_screen(self):
         downsample_size = 80, 60
         if os.path.isdir(args.path):
+#            if is_capture_env :
+#                state_path = os.path.join(args.path, '../env.png')                
+#                cp_path = os.path.join(args.path, self.state_path)
+#                copyfile(state_path, cp_path)
             state_path = os.path.join(args.path, self.state_path)
+
             if os.path.isfile(state_path):
                 try:
                     state_img = Image.open(state_path).convert('L') # Open as grayscale
@@ -124,13 +148,24 @@ class LearningAgent(Agent):
 #                    state = state.unsqueeze(0) # Add channel dimension - (C, H, W)
 #                    print "\timage =>", state
                     state = state.type('torch.FloatTensor')
+#                    state = torchvision.transforms.Normalize(0.0, 1.0)(state)
+                    state = state/255.
+#                    torchvision.utils.save_image(state, './data/_tmp.jpg')
                     state = state.view(1,60,80).to(self.device)
+#                    for _ in range(60) :
+#                        print "\timage", state[0][_]
 #                    print "\timage_shape =>", state.size()
                 except IOError:
                     print ("Could not open '%s'" % state_path)
+            else:
+                print_log("Error : there is no state_path => {}".format(state_path), log)
+        else :
+            print_log("Error : there is no args.path => {}".format(args.path), log)
+
+        self.env.state_img = state
 
         return state
-
+    '''
     def update_epsilon(self):
         if self.epsilon >= self.epsilon_end:
             self.epsilon *= self.epsilon_decay
@@ -139,8 +174,8 @@ class LearningAgent(Agent):
     def optimize_model(self, memory):
         if len(memory.memory) < args.BATCH_SIZE :
             return
-        transitions = memory.sample(args.BATCH_SIZE, self.time_backward)
-        print "transition sample: ", transitions[3]
+        transitions = memory.sample(args.BATCH_SIZE)
+#        print "transition ", transitions.size()
         batch = Transition(*zip(*transitions))
 #        print "\ttransition \t", batch
 
@@ -157,11 +192,14 @@ class LearningAgent(Agent):
         a_batch = torch.cat(batch.action).view(args.BATCH_SIZE, -1)
         r_batch = torch.cat(batch.reward)
 
-#        print "a_batch ", a_batch.size()
+#        print "a_batch.size : ", a_batch.size()
+#        print "\ta_batch : ", a_batch
 
         Q = self.policy_net(s_img_batch, s_tuple_batch)
 #        print "QQQQQ ", Q.data.size()
+#        print "\tQ : ", Q
         Q = Q.gather(1, a_batch)
+#        print "\tQ_gather : ", Q
 
 #        s_img = state[0]
 #        s_v = torch.Tensor([state[1] for i in range(BATCH_SIZE)], device=self.device).view(BATCH_SIZE, -1)
@@ -179,8 +217,11 @@ class LearningAgent(Agent):
 #        Q = Q.index_select(dim=0, index=action)
 #        print "Q_ind", Q
         V_next = torch.zeros(args.BATCH_SIZE, device=self.device)
-        V_next[non_final_mask] = self.target_net(non_final_next_s_img, non_final_next_s_tuple).max(1)[0].detach()
+        V_next[non_final_mask] = self.target_net(
+                non_final_next_s_img, non_final_next_s_tuple
+                ).max(1)[0].detach()
 
+#        print_log("\treward : {}".format(r_batch), log)
         expected_Q = (V_next * self.gamma) + r_batch
 
         loss = F.smooth_l1_loss(Q, expected_Q.unsqueeze(1))
@@ -193,10 +234,10 @@ class LearningAgent(Agent):
 
 
     def update(self):
-        if self.state == None:
+        if self.state == None: # init
             agent_pose = self.env.sense()
             self.state = state(
-                    state_img = self.get_screen(),
+                    state_img = self.env.get_screen().to(self.device),
                     state_tuple = torch.Tensor(state_tuple(
                         x = agent_pose[0],
                         y = agent_pose[1],
@@ -210,16 +251,16 @@ class LearningAgent(Agent):
         if self.env.enforce_deadline:
             deadline = self.env.get_deadline()
 
+        self.env._image_show()
         # Select action according to your policy
         action = self.get_action(self.state)
-        #print "action: " + str(action) + "\n"
-        #time.sleep(5)
-        # print 'max_q_value:',max_q_value
-        
         # Execute action and get reward
         next_agent_pose,reward = self.env.act(self, action)
+        # s_img update : screen plt update
+        self.env.update_screen()
+        
         self.next_state = state(
-                state_img = self.get_screen(),
+                state_img = self.env.get_screen().to(self.device),
                 state_tuple = torch.Tensor(state_tuple(
                     x=next_agent_pose[0], 
                     y=next_agent_pose[1], 
@@ -248,9 +289,9 @@ class LearningAgent(Agent):
         if random.random() > self.epsilon :
             with torch.no_grad():
                 q_values = self.policy_net(s_img, s_tuple)
-                print "\tq_values", q_values
+#                print "\tq_values", q_values
                 action_selected = car_sim_env.valid_actions[q_values.argmax(dim=1, keepdim=False)]
-                print "\tget_action : act", (action_selected)
+#                print "\tget_action : act", (action_selected)
         else :
             action_selected = random.choice(car_sim_env.valid_actions)
 
@@ -263,16 +304,59 @@ def run(restore):
     env.set_agent(agt, enforce_deadline=False)
 
 
-#    train_thread = threading.Thread(name="train", target=train, args=(env, agt, restore))
-#    train_thread.daemon = True
-#    train_thread.start()
-    
-    train(env, agt, restore)
+    #train_thread = threading.Thread(name="train", target=train, args=(env, agt, restore))
+    #train_thread.daemon = True # exit sub_threading when main_threading is done
+    #train_thread.start()
+   
+    #plt_thread = threading.Thread(name="env.plt_show()", target=env.plt_show(), args=())
+    #plt_thread.daemon = True
+    #plt_thread.start()
+
     env.plt_show()
-    #train(env, agt, restore)
+    train(env, agt, restore)
     while True:
         continue
 
+def _replayMemoryImageCheck_forTest(agt, dir_idx) :
+    m = agt.memory
+    DATA_DIR_CUR = './data/{}_replaymemory_cur'.format(dir_idx)
+    DATA_DIR_NEXT = './data/{}_replaymemory_next'.format(dir_idx)
+
+    try:
+        if not os.path.isdir(DATA_DIR_CUR) :
+            print "Data directory does not exist: Invalid."
+            print "Creating ./data directory..."
+            os.mkdir(DATA_DIR_CUR)
+        
+        if not os.path.isdir(DATA_DIR_NEXT) :
+            #torchvision.transforms.ToPILImage()(x)
+            print "Data directory does not exist: Invalid."
+            print "Creating ./data directory..."
+            os.mkdir(DATA_DIR_NEXT)
+
+        if 1 :
+#            x = []
+            for i in range(len(m.memory)):
+                x_c = m.memory[i].state.state_img
+                #x_t = m.memory[i][0][0]
+                save_path_cur = os.path.join(DATA_DIR_CUR, 'state{}.png'.format(i))
+                torchvision.utils.save_image(x_c, save_path_cur)
+                
+                x_n = m.memory[i].next_state.state_img
+                save_path_next = os.path.join(DATA_DIR_NEXT, 'state{}.png'.format(i))
+                torchvision.utils.save_image(x_n, save_path_next)
+                #x_t = T.functional.to_pil_image(x_t)
+                #x.append(x_t)
+            
+#            state = os.path.join(DATA_DIR, 'state{}.png'.format(idx))
+#            idx = 0
+#            while not os.path.isfile(state) :
+#                self.env_fig.savefig(state)
+#                idx += 1
+#                state = os.path.join(DATA_DIR, 'state{}.png'.format(idx))
+
+    except OSError:
+        print "mkdir failed: Creating a new dir failed."
 
 def train(env, agt, restore):
     n_trials = 9999999999
@@ -280,7 +364,7 @@ def train(env, agt, restore):
     max_index = 0
     for trial in xrange(max_index + 1, n_trials):
         # time.sleep(3)
-        #print "Simulator.run(): Trial {}".format(trial)  # [debug]
+        print "\tSimulator.run(): Trial {}".format(trial)  # [debug]
         if not agt.test:
             if trial > 80000 and trial < 150000:
                 agt.epsilon = 0.3
@@ -291,9 +375,9 @@ def train(env, agt, restore):
 
         env.reset()
 
-
         while True:
             try:
+                agt.update()
                 env.step()
             except KeyboardInterrupt:
                 quit = True
@@ -304,8 +388,9 @@ def train(env, agt, restore):
         if trial % args.TARGET_UPDATE_CYCLE == 0 :
             agt.target_net.load_state_dict(agt.policy_net.state_dict())
 
-        test_interval = 100
-        if trial % test_interval == 0:
+        if trial % args.TEST_INTERVAL == 0:
+            _replayMemoryImageCheck_forTest(agt, trial)
+
             total_runs = env.succ_times + env.hit_wall_times + env.hit_car_times + env.num_hit_time_limit \
                          + env.num_out_of_time
             succ_rate = env.succ_times / float(total_runs)
